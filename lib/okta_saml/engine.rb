9811e27e865cf6170810aa5153daf99b3b9ea919
Jeb Beich <jebbeich@gmail.com>
2013-10-02 15:51:54 +0000

    [EL/JEB] [57352336] added initial oauth mapping flow

--- lib/okta_saml/engine.rb
+++ lib/okta_saml/engine.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'ruby-saml'
+require "net/http"
+require "uri"
 require_relative 'session_helper'
 
 class ActionController::Base
@@ -7,13 +9,71 @@ class ActionController::Base
 
   def okta_authenticate!
     session[:redirect_url] = params[:app_referer] || "#{request.protocol}#{request.host_with_port}#{request.fullpath}"
-    redirect_to saml_init_path unless signed_in?
+    auth_code = params[:auth_code]
+
+    # if no auth_code from propsol, auth using okta
+    if auth_code.blank?
+      redirect_to saml_init_path unless signed_in?
+
+    else
+      ps_user_id = get_user_id(auth_code)
+      email = get_cr3_email(ps_user_id)
+
+      # We know:
+      # * auth_code presence in request
+      # * email (not nil) indicates PS->CR3 mapping exists
+      # * okta_signed_in
+
+      if email.present?
+        # They have auth_code and mapping already exists (since email present)
+        # so log them in.
+        sign_in(OktaUser.new({:email => email}))
+
+      else # no mapping exists
+
+        if signed_in? # if already signed into okta, but does have
+                      # auth_code create the mapping, and log them in.
+          create_ps_to_cr3_mapping(ps_user_id, email)
+          sign_in(OktaUser.new({:email => email}))
+
+        else # since not signed into okta, send them to okta login.
+          redirect_to saml_init_path
+        end
+      end
+    end
   end
 
   def okta_logout
     redirect_to saml_logout_path
   end
 
+  def create_ps_to_cr3_mapping(ps_user_id, email)
+    randr_uri = Rails.application.config.randr_service
+    randr_uri += "/portalsvc/proplsol/add-user-mapping?ps-user-id=" + ps_user_id
+    res = http_get(randr_uri) # { "result": "true" } 
+    res.body["email"]
+  end
+
+  def get_cr3_email(ps_user_id)
+    randr_uri = Rails.application.config.randr_service
+    randr_uri += "/portalsvc/proplsol/get-cr3-user?ps-user-id=" + ps_user_id
+    res = http_get(randr_uri) # { "email": "foo@bar.com" or "" }
+    res.body["email"]
+  end
+
+  def get_user_id(auth_code)
+    randr_uri = Rails.application.config.randr_service
+    randr_uri += "/portalsvc/proplsol/get-ps-user-id?auth-code=" + auth_code
+    res = http_get(randr_uri) # { "user-id": "-1" }
+    res.body["user-id"]
+  end
+
+  def http_get(uri)
+    url = URI.parse(randr_uri)
+    req = Net::HTTP::Get.new(url.path)
+    res = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
+    puts res.body
+  end
 end
 
 module OktaSaml
